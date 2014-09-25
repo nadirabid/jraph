42b2c518a2dfaa72791c87c91148cbdfdfacb3c1
Nadir Muzaffar <nadirabid@gmail.com>
2014-09-25 04:29:42 -0700

    removing target files from repo

--- target/web/public/main/js/globals.js
+++
@@ -1,8 +0,0 @@
-// Globals - with a shared nature
-
-var HALF_PI = Math.PI / 2;
-var TWO_PI = Math.PI * 2;
-var E_MINUS_1 = Math.E - 1;
-var MOUSE_LEFT_BUTTON = 0;
-
-document.mouse = { state: 'initial', data: { } };
