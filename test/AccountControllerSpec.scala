d408391dce97f5b26cf9cb4f18b4ff1adefbede4
Nadir Muzaffar <nadirabid@gmail.com>
2014-12-05 11:40:59 -0800

    adding AccountController tests

---
+++ test/AccountControllerSpec.scala
@@ -0,0 +1,43 @@
+import java.util.UUID
+
+import com.mohiva.play.silhouette.api.LoginInfo
+import com.mohiva.play.silhouette.impl.authenticators.SessionAuthenticator
+import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
+import com.mohiva.play.silhouette.test._
+import models.User
+import org.scalatest._
+import org.scalatestplus.play._
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.libs.json._
+
+// By default the tests of a given ScalaTest Suite (WordSpec, FlatSpec, etc)
+// are run sequentially inferred from the order in which the tests are defined.
+
+class AccountControllerSpec extends WordSpec
+  with ShouldMatchers
+  with OneAppPerTest {
+
+  val userEmail = UUID.randomUUID().toString + "@test.com"
+
+  "The Account controller" should {
+    "should create a new user given unique username and password" in {
+      val userCreateRequest = FakeRequest(POST, "/account/create")
+        .withFormUrlEncodedBody(Map("email" -> userEmail, "password" -> "123").toSeq:_*)
+
+      val userCreateResult = route(userCreateRequest).get
+      status(userCreateResult) shouldBe SEE_OTHER //create does a redirect on authentication
+    }
+
+    "should delete the newly created user account" in {
+      val identity = User(userEmail, LoginInfo(CredentialsProvider.ID, userEmail))
+      implicit val env = FakeEnvironment[User, SessionAuthenticator](identity)
+
+      val userDeleteRequest = FakeRequest(DELETE, "/account/delete")
+        .withAuthenticator(identity.loginInfo)
+
+      val userDeleteResult = route(userDeleteRequest).get
+      status(userDeleteResult) shouldBe OK
+    }
+  }
+}
