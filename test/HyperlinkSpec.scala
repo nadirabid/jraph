37904563bb0f0fc7a459252b30cc0fdbed8b8f14
Nadir Muzaffar <nadirabid@gmail.com>
2015-05-03 19:17:58 -0700

    increase Await timeout in HyperlinkSpec

--- test/HyperlinkSpec.scala
+++ test/HyperlinkSpec.scala
@@ -179,8 +179,8 @@ class HyperlinkSpec extends WordSpec
         Some(Json.obj("p1" -> "v1"))
       )
 
-      Await.result(Hypernode.create(userEmail, defaultHypergraph.id, sourceNode), 200.millis)
-      Await.result(Hypernode.create(userEmail, defaultHypergraph.id, targetNode), 200.millis)
+      Await.result(Hypernode.create(userEmail, defaultHypergraph.id, sourceNode), 2000.millis)
+      Await.result(Hypernode.create(userEmail, defaultHypergraph.id, targetNode), 2000.millis)
 
       val hyperlink = Hyperlink(
         UUID.randomUUID(),
