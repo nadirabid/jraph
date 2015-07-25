987f49d20338ad85054f3d9464301c4af3b9f5cd
Nadir Muzaffar <nadirabid@gmail.com>
2015-07-24 18:22:17 -0700

    update_server.py: output progress from Repo.clone_from

--- scripts/update_server.py
+++ scripts/update_server.py
@@ -3,19 +3,26 @@
 import os
 import subprocess
 from git import Repo
+from git.remote import RemoteProgress
 
-print "Cloning repository..."
+class Progress(RemoteProgress):
+    def line_dropped(self, line):
+        print line
+    def update(self, *args):
+        print self._cur_line
+
+print "\n ### Cloning repository..."
 
 target_dir = "hypergraph"
 
-Repo.clone_from("git@github.com:nadirabid/hypergraph.git", target_dir)
+Repo.clone_from("git@github.com:nadirabid/hypergraph.git", target_dir, progress=Progress())
 
-print "Building project..."
+print "\n ### Building project..."
 
 os.chdir(target_dir)
 
-print subprocess.Popen(".activator -J-Xms256m -J-Xmx256m clean stage", shell=True, stdout=subprocess.PIPE).stdout.read()
+print subprocess.Popen("./activator -J-Xms256m -J-Xmx256m clean stage", shell=True, stdout=subprocess.PIPE).stdout.read()
 
 os.chdir("..")
 
-print "Finished"
+print "\n ### Finished"
