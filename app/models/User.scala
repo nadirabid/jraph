906ca956b1fe4830e062284100b8e1e4c4a03a8d
Nadir Muzaffar <nadirabid@gmail.com>
2015-07-27 16:27:26 -0700

    move User Json Reads back into UserDAOImpl - was causing errors

--- app/models/User.scala
+++ app/models/User.scala
@@ -3,10 +3,6 @@ package models
 import com.mohiva.play.silhouette.api.{Identity, LoginInfo}
 import java.util.UUID
 
-import play.api.libs.json.Reads._
-import play.api.libs.json.{JsPath, Reads}
-import play.api.libs.functional.syntax._
-
 case class User(
     id: UUID,
     email: String,
@@ -15,14 +11,3 @@ case class User(
     loginInfo: LoginInfo,
     role: Option[String] = None
 ) extends Identity
-
-object User {
-    implicit val userReads: Reads[User] = (
-      (JsPath \ "id").read[UUID] and
-        (JsPath \ "email").read[String](email) and
-        (JsPath \ "firstName").readNullable[String] and
-        (JsPath \ "lastName").readNullable[String] and
-        JsPath.read[LoginInfo] and
-        (JsPath \ "role").readNullable[String]
-      )(User.apply _)
-}
