f74b53ed9d4f53f9b92ed66692bd5fae765a0953
Nadir Muzaffar <nadirabid@gmail.com>
2015-08-03 11:05:55 -0700

    add gzip filter

--- app/core/Filters.scala
+++ app/core/Filters.scala
@@ -6,11 +6,14 @@ import play.api.http.HttpFilters
 import play.api.mvc.EssentialFilter
 import play.filters.csrf.CSRFFilter
 import play.filters.headers.SecurityHeadersFilter
+import play.filters.gzip.GzipFilter
 
 /**
  * Provides filters.
  */
-class Filters @Inject() (csrfFilter: CSRFFilter, securityHeadersFilter: SecurityHeadersFilter)
+class Filters @Inject() (csrfFilter: CSRFFilter,
+                         gzipFilter: GzipFilter,
+                         securityHeadersFilter: SecurityHeadersFilter)
   extends HttpFilters {
-  override def filters: Seq[EssentialFilter] = Seq(csrfFilter)
+  override def filters: Seq[EssentialFilter] = Seq(gzipFilter, csrfFilter)
 }
