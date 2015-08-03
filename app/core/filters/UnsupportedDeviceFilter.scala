91341644b01015b65b46c8a1d3c1c1400e19e5d2
Nadir Muzaffar <nadirabid@gmail.com>
2015-08-03 11:39:53 -0700

    minor refactor in UnsupportedDeviceFilter

--- app/core/filters/UnsupportedDeviceFilter.scala
+++ app/core/filters/UnsupportedDeviceFilter.scala
@@ -8,12 +8,12 @@ class UnsupportedDeviceFilter extends Filter with Results {
   def apply(nextFilter: RequestHeader => Future[Result])
            (requestHeader: RequestHeader): Future[Result] = {
 
-    val Pattern = "(iPhone|webOS|iPod|Android|BlackBerry|mobile|SAMSUNG|IEMobile|OperaMobi)".r.unanchored
+    val MobileDevicePattern = "(iPhone|webOS|iPod|Android|BlackBerry|mobile|SAMSUNG|IEMobile|OperaMobi)".r.unanchored
 
     val userAgent = requestHeader.headers.get("User-Agent")
 
     userAgent match {
-      case Some(Pattern(a)) => Future.successful(Ok(views.html.unsupportedDevice()))
+      case Some(MobileDevicePattern(a)) => Future.successful(Ok(views.html.unsupportedDevice()))
       case Some(b) => nextFilter(requestHeader)
       case None => nextFilter(requestHeader)
     }
