2c87b4f905dfab198168e10371603e3eaa1f91a2
Nadir Muzaffar <nadirabid@gmail.com>
2015-08-07 22:19:00 -0700

    update_server: saving various changes - managing app folders

--- scripts/update_server.py
+++ scripts/update_server.py
@@ -2,10 +2,13 @@
 
 import os
 import subprocess
+import glob
 from git import Repo
 from git.remote import RemoteProgress
 
 ### SOME DEFINITIONS
+
+BASE_APP_DIR_NAME = "jraph_"
 
 class Progress(RemoteProgress):
     def line_dropped(self, line):
@@ -35,43 +38,40 @@ def hilite(string, status, bold):
         attr.append('1')
     return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
 
-def app_dir_name(base_dir_name, dir_counter):
-    return base_dir_name + "_" + str(dir_counter)
+def app_dir_name(dir_version):
+    return BASE_APP_DIR_NAME + "_" + str(dir_version)
+
+def find_most_recent_app_dir_version():
+    app_dirs_list = glob.glob(BASE_APP_DIR_NAME + "*")
+    sorted_app_dir_versions_list = sorted(map(lambda app_dir: int(app_dir.split("_")[1]), app_dirs_list))
+    return max(sorted_app_dir_versions_list)
+
 
 
 ### SCRIPT	STARTS	HERE
 
+previous_app_dir_version = find_most_recent_app_dir_version()
+previous_app_dir_name = app_dir_name(previous_app_dir_version)
+
 print hilite("\nFinding directory to clone repository to...", True, True)
 
-target_dir_base = "jraph"
-target_dir_counter = 0
-target_dir = app_dir_name(target_dir_base, target_dir_counter)
-
-while os.path.exists(target_dir):
-    target_dir_counter += 1
-    target_dir = app_dir_name(target_dir_base, target_dir_counter)
+current_app_dir_version = previous_app_dir_version + 1
+current_app_dir_name = app_dir_name(current_app_dir_version)
 
 
 
-print hilite("Cloning repository to " + target_dir + "...", True, True)
+print hilite("Cloning repository to " + current_app_dir_name + "...", True, True)
 
-Repo.clone_from("git@github.com:nadirabid/jraph.git", target_dir, progress=Progress())
+Repo.clone_from("git@github.com:nadirabid/jraph.git", current_app_dir_name, progress=Progress())
 
 
 
 print hilite("\nShutting down current running server...", True, True)
 
-old_target_dir = app_dir_name(target_dir_base, 0)
-if os.path.exists(old_target_dir + "/target/universal/stage/RUNNING_PID"):
-    os.chdir(old_target_dir)
-    execute("./activator stopProd -J-Xms64m -J-Xmx256m")
-    os.chdir("..")
-
-
 
 print hilite("\nBuilding project...", True, True)
 
-os.chdir(target_dir)
+os.chdir(current_app_dir_name)
 execute("./activator -J-Xms256m -J-Xmx256m clean stage")
 
 
