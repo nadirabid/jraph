a1bc0e007d44c7e7678fd2b5e957dd07fd3c8669
Nadir Muzaffar <nadirabid@gmail.com>
2015-08-18 13:17:55 -0700

    rip out GraphComponent from app.js into its own file

--- app/assets/js/graph/components/GraphComponent.js
+++ app/assets/js/graph/components/GraphComponent.js
@@ -1,54 +1,11 @@
 define([
-    'lodash',
-    'jquery',
-    'mousetrap',
     'vue',
-    'd3',
-    'shared/util',
-    'shared/daos/HypergraphDAO',
-    'shared/daos/NodeDAO',
-    'shared/daos/EdgeDAO',
-    'graph/state',
-    'graph/components/NavComponent',
-    'graph/components/NodeComponent',
-    'graph/components/EdgeComponent',
-    'graph/components/GraphControlsComponent',
-    'graph/components/ContextMenuComponent',
-    'graph/components/NodePanelComponent',
-    'graph/components/ForceLayoutPanel'
-], function (
-    _,
-    $,
-    Mousetrap,
-    Vue,
-    d3,
-    util,
-    HypergraphDAO,
-    NodeDAO,
-    EdgeDAO,
-    State,
-    NavComponent,
-    NodeComponent,
-    EdgeComponent,
-    GraphControlsComponent,
-    ContextMenuComponent,
-    NodePanelComponent,
-    ForceLayoutPanel
-) {
+    'jquery',
+    'shared/util'
+], function(Vue, $, Util) {
   'use strict';
 
-  ///
-  /// DEFINITIONS
-  ///
-
-  var edgesMap = Object.create(null);
-  var nodeComponentsMap = Object.create(null);
-
-  var state = new State();
-
-  var hypergraphID = _graph.id;
-
-  var GraphComponent = Vue.extend({
+  return Vue.extend({
 
     template: document.getElementById('graph').innerHTML,
 
@@ -70,7 +27,7 @@ define([
     data: function () {
       return {
         isZooming: false,
-        state: state,
+        state: null,
         nodes: [],
         edges: [],
         width: 0,
@@ -121,13 +78,12 @@ define([
             .translate(-p.x, -p.y);
 
         Vue.nextTick(function() {
-          util.setCTM(nodesAndLinksGroupEl, k);
+          Util.setCTM(nodesAndLinksGroupEl, k);
         });
 
         if (!optimizeForSpeed) {
           return;
         }
-
 
         // set isZooming to true and automatically reset it to false
         // after 100ms of idle zoom ... the isZooming flag is used
@@ -180,11 +136,11 @@ define([
 
         var ctm = this._ctm;
 
-        var v = util.transformVectorFromClientToEl(e.dx, e.dy, this.$$.nodesAndLinksGroup);
+        var v = Util.transformVectorFromClientToEl(e.dx, e.dy, this.$$.nodesAndLinksGroup);
 
         var self = this;
         Vue.nextTick(function() {
-          util.setCTM(self.$$.nodesAndLinksGroup, ctm.translate(v.x, v.y));
+          Util.setCTM(self.$$.nodesAndLinksGroup, ctm.translate(v.x, v.y));
         });
       },
 
@@ -206,7 +162,7 @@ define([
         var defaultCTM = this.$el.createSVGMatrix();
 
         Vue.nextTick(function() {
-          util.setCTM(self.$$.nodesAndLinksGroup, defaultCTM);
+          Util.setCTM(self.$$.nodesAndLinksGroup, defaultCTM);
         });
       },
 
@@ -310,9 +266,11 @@ define([
       this.$options.hypergraphID = this.$parent.$options.hypergraphID;
       this.$options.edgeContextMenu = this.$parent.$options.edgeContextMenu;
 
+      this.state = this.$parent.$options.state;
+
       this.resize();
 
-      var $svg = util(this.$el);
+      var $svg = Util(this.$el);
       $svg.on('dragstart', this.panStart.bind(this));
       $svg.on('drag', this.pan.bind(this));
       $svg.on('dragend', this.panEnd.bind(this));
@@ -334,185 +292,4 @@ define([
 
   });
 
-  ///
-  /// MAIN APP CODE
-  ///
-
-  var graphContextMenu = new ContextMenuComponent({
-
-    methods: {
-
-      addNode: function(e) {
-        app.newNode(e.clientX, e.clientY);
-      },
-
-      saveGraph: function() {
-        app.saveAllGraphData();
-      }
-
-    }
-
-  });
-
-  var edgeContextMenu = new ContextMenuComponent({
-
-    methods: {
-
-      beforeShow: function(x, y, link) {
-        this.$.link = link;
-      },
-
-      afterHide: function() {
-        this.$.link = null;
-      },
-
-      delete: function() {
-        this.$.link.delete();
-      }
-
-    }
-
-  });
-
-  Vue.component('x-force-layout-panel', ForceLayoutPanel);
-  Vue.component('x-node-panel', NodePanelComponent);
-  Vue.component('x-graph-controls', GraphControlsComponent);
-  Vue.component('x-graph', GraphComponent);
-  Vue.component('x-nav', NavComponent);
-  Vue.component('x-edge', EdgeComponent);
-  Vue.component('x-node', NodeComponent);
-
-  graphContextMenu.$mount('#graphContextMenu');
-  edgeContextMenu.$mount('#edgeContextMenu');
-
-  function ForceLayoutSettings() {
-    this.isPanelOpen = true;
-    this.isRunning = false;
-
-    this.defaultParameters = {
-      alpha: 0.1,
-      theta: 0.1,
-      friction: 0.8,
-      gravity: 0.15,
-      charge: -20000,
-      chargeDistance: 1600,
-      linkDistance: 200,
-      linkStrength: 10
-    };
-
-    this.parameters = _.clone(this.defaultParameters);
-  }
-
-  var app = new Vue({
-
-    el: '#all',
-
-    state: state,
-
-    edgesMap: edgesMap,
-
-    nodeComponentsMap: nodeComponentsMap,
-
-    hypergraphID: hypergraphID,
-
-    edgeContextMenu: edgeContextMenu,
-
-    data: {
-      hypergraphID: hypergraphID,
-      dataState: 'SAVED', // UNSAVED/SAVING/SAVED
-      graph: _graph, // _graph is bootstrapped into the main.scala.html view
-      nodes: [],
-      edges: [],
-      nodeInfoToDisplay: null,
-      forceLayout: new ForceLayoutSettings()
-    },
-
-    methods: {
-
-      newNode: function(x, y) {
-        var graphComponent = this.$.graphComponent;
-
-        var ctm = graphComponent.$$.nodesAndLinksGroup.getScreenCTM();
-        var p = graphComponent.$el.createSVGPoint();
-        p.x = x;
-        p.y = y;
-        p = p.matrixTransform(ctm.inverse());
-
-        var nodeData = {
-          x: p.x,
-          y: p.y,
-          fixed: false,
-          isNew: true,
-          hasChanges: false,
-          markedForDeletion: false,
-          data: {
-            name: 'Node Name',
-            properties: {}
-          }
-        };
-
-        var nodeComponent = graphComponent
-            .$addChild({ data: nodeData}, NodeComponent)
-            .$mount()
-            .$appendTo(graphComponent.$$.dynamicContent);
-
-        nodeComponent.$watch('markedForDeletion', function(markedForDeletion) {
-          if (markedForDeletion) {
-            nodeComponent.$destroy(true);
-          }
-        });
-
-        this.nodeInfoToDisplay = nodeData;
-      },
-
-      incrementZoom: function() {
-        this.$.graphComponent.incrementZoomLevel();
-      },
-
-      decrementZoom: function() {
-        this.$.graphComponent.decrementZoomLevel();
-      },
-
-      centerView: function() {
-        this.$.graphComponent.centerView();
-      },
-
-      saveAllGraphData: function() {
-        var updateNodesPromise = NodeDAO.update(hypergraphID, this.nodes);
-        //var updateEdgesPromise = EdgeDAO.update(hypergraphID, this.nodes);
-        var updateGraphPromise = HypergraphDAO.update(this.graph);
-
-        var self = this;
-        this.dataState = 'SAVING';
-
-        return $
-            .when(updateGraphPromise, updateNodesPromise)
-            .then(function() {
-              self.dataState = 'SAVED';
-            });
-      }
-
-    }
-
-  });
-
-  // fetch data
-
-  util.when(NodeDAO.fetchAll(hypergraphID), EdgeDAO.fetchAll(hypergraphID))
-      .done(function (nodes, links) {
-        nodes.forEach(function (n) {
-          links.forEach(function (l) {
-            if (l.sourceId == n.id) l.source = n;
-            if (l.targetId == n.id) l.target = n;
-
-            var targets = edgesMap[l.sourceId] || (edgesMap[l.sourceId] = {});
-            targets[l.targetId] = l;
-          });
-        });
-
-        app.nodes = nodes;
-        app.edges = links;
-
-        app.$.graphComponent.$forceLayout.stop();
-      });
 });
